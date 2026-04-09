e1a2fd81f0d14cdc06f4b9ad2e15015c3c81594e
Misbahul Kadery <kaderymisbahul@gmail.com>
2026-04-09 19:01:56 +0530

    9th Apr, 2026

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,10 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+
+const createUser = async (req: FastifyRequest, res: FastifyReply) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
