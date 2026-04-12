d471d44fea48b9468cebc807fc03b24ae1fb9bdd
RG-67 <rittikghoshkolkata@gmail.com>
2026-04-12 21:43:03 +0530

    12th Apr, 2026 - main

--- src/middlewares/auth.hook.ts
+++ src/middlewares/auth.hook.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
-
+// Role checking should be done here
 export const AuthHook = async (req: FastifyRequest, res: FastifyReply, next: any) => {
     try {
         const token = req.headers.authorization?.split(" ")[1];
