c2144ba606c7c8d9e5f4ba3e48185463617dd3e6
RG-67 <rittikghoshkolkata@gmail.com>
2026-04-13 09:45:45 +0530

    13th Apr, 2026 - main

--- src/middlewares/auth.hook.ts
+++ src/middlewares/auth.hook.ts
@@ -1,9 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import dotEnv from 'dotenv';
+import jwt from 'jsonwebtoken';
 
-// Role checking should be done here
+dotEnv.config();
+
 export const AuthHook = async (req: FastifyRequest, res: FastifyReply, next: any) => {
     try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            return res.code(401).send({ status: false, message: "No token provided" });
+        }
         const token = req.headers.authorization?.split(" ")[1];
+        const decoded = jwt.verify(token as string, process.env.JWT_SECRET_KEY as string);
+        console.log("Decoded: ", decoded);
+        if (decoded) {
+            
+        }
     } catch (error) {
 
     }
