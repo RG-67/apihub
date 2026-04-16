28f3d3e42879513a2ed37c980f97c09584231f44
RG-67 <rittikghoshkolkata@gmail.com>
2026-04-16 21:27:20 +0530

    16th Apr, 2026 - main

--- src/modules/api/api.repository.ts
+++ src/modules/api/api.repository.ts
@@ -11,7 +11,7 @@ export class ApiRepository {
     async createApi(providerApi: providerApi) {
         const api = await this.db.query(`
                 INSERT INTO apis (provider_id, name, description, base_url, endpoint, method, price, rate_limit, status, is_public)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`,
             [providerApi.providerId, providerApi.name, providerApi.description, providerApi.baseUrl, providerApi.endPoint, providerApi.method,
             providerApi.price, providerApi.rateLimit, "inactive", false
             ]);
